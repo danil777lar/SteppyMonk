a268c5693888e8372af97a175b07d0108e9b549a
danil777lar <danil777lar@gmail.com>
2021-02-08 14:37:10 +0300

    scene transition

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    private static SceneTransition instance;
+
+    private Animator anim;
+    private AsyncOperation operation;
+
+    void Start()
+    {
+        instance = this;
+
+        anim = GetComponent<Animator>();
+        anim.Play("Base Layer.End");
+    }
+
+    public void SwitchScene(string sceneName){
+        anim.Play("Base Layer.Start");
+        operation = SceneManager.LoadSceneAsync("Scenes/"+sceneName);
+        operation.allowSceneActivation = false;
+    }
+
+    public void AnimOver(){
+        operation.allowSceneActivation = true;
+    }
+}
