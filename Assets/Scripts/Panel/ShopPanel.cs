28680fadf3de511d30bd5fe68aff7ea3191c349e
danil777lar <danil777lar@gmail.com>
2021-02-08 18:15:12 +0300

    shop and inventory works

---
+++ Assets/Scripts/Panel/ShopPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopPanel : Panel
+{
+    public override void Close(){
+        base.Close();
+        GetComponentInParent<GameStateManager>().cam.GetComponent<Animator>().Play("Base Layer.ToInventory");
+    }
+
+    public override void Open()
+    {
+        base.Open();
+        GetComponentInParent<GameStateManager>().cam.GetComponent<Animator>().Play("Base Layer.ToShop");
+    }
+}
